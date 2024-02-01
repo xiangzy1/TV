bacdc72855808a5d1145569b02bb520498bb27e1
xiangzy1 <xiangzy8@gmail.com>
2024-02-01 15:05:16 +0800

    update

---
+++ sync.sh
@@ -0,0 +1,5 @@
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/joevess/IPTV/main/sources/iptv_sources.m3u8 -o libs/joevess.m3u
+curl -s https://raw.githubusercontent.com/Meroser/IPTV/main/IPTV.m3u -o libs/Meroser.m3u
+curl -s https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u -o libs/aptv.m3u
+curl -s https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u -o libs/yuechan.m3u
+curl -s https://cdn.jsdelivr.net/gh/BurningC4/Chinese-IPTV@master/TV-IPV4.m3u -o libs/BurningC4.m3u
